b815b43e952b6d63e2618ac052777e5ff7ece412
nobbyIU <norbert.fekete@iubh-fernstudium.de>
2024-09-11 13:43:11 +0200

    Organize project structure: add assets, js, and css directories
    - Created new directories for better project organization:
      - /assets for images and other media files
      - /css for stylesheets
      - /js for JavaScript files

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*
+    Datei: script.js
+    Beschreibung: JavaScript für die interaktive Logik der Webseite.
+    Erstellt von: Norbert Fekete
+    Erstelldatum: 11.09.2024
+    Letzte Änderung: 11.09.2024
+*/
