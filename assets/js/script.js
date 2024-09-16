e5f4fe25940937b10e7dfd829f3ffc386103723e
nobbyIU <norbert.fekete@iubh-fernstudium.de>
2024-09-16 14:02:30 +0200

    Made navigation menu responsive
    - updated link styles
    - added JavaScript for mobile toggle
    - djusted CSS for better layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,13 @@
     Erstelldatum: 11.09.2024
     Letzte Änderung: 11.09.2024
 */
+
+/* Toggle between adding and removing the "responsive" class to mainnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("mainnav");
+    if (x.className === "mainnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mainnav";
+    }
+}
