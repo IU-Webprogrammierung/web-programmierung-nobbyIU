62f29ddf3eadb29bc0b20d055815942963604ceb
nobbyIU <norbert.fekete@iubh-fernstudium.de>
2024-10-10 09:51:46 +0200

    Refactored JavaScript and updated CSS styles
    - Moved JavaScript logic for the image slideshow into an external file (script.js) for better code organization.
    - Added a smooth fade transition for the background image in the slideshow.
    - Overwritten and updated the old style.css to optimize the overall design with Tailwind CSS and enhance visual consistency.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@
     Erstelldatum: 11.09.2024
     Letzte Änderung: 11.09.2024
 */
+
+// Slideshow Hauptseite
+const images = [
+    "assets/img/heroimage1.jpg",
+    "assets/img/heroimage2.jpg",
+    "assets/img/heroimage3.jpg"
+];
+
+let currentImageIndex = 0;
+const heroDiv = document.getElementById('hero');
+
+setInterval(() => {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    heroDiv.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+}, 3000);
